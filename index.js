7c44cffba4408856db6e5978c3a252202d8a7d05
akkhara <p.akkhara@gmail.com>
2024-08-22 12:47:22 +0700

    add cors support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require("cors");
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.post('/login', (req, res) => {
     let data = req.body;
